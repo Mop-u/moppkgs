828ed8277f6d0b00a9dcac6b872b98248da55ab1
Quinn Unger <quinn.unger@pm.me>
2026-02-19 16:21:57 +0000

    add quartus lite version 25.1std.0.1129

--- pkgs/programs/quartus/sources/lite.nix
+++ pkgs/programs/quartus/sources/lite.nix
@@ -45,4 +45,24 @@ in
         };
     };
 
+    "25.1std.0.1129" = {
+        variant = "lite";
+        version = "25.1std.0.1129";
+        baseUrl = "https://downloads.intel.com/akdlm/software/acdsinst/25.1std/1129/ib_installers";
+        quartusInstaller = "QuartusLiteSetup";
+        defaultInstalls = [ ];
+        defaultDevices = [ "cyclonev" ];
+        installers = {
+            QuartusLiteSetup = "sha256-UYQz7H3NYXJVYK9lM1P3pcMgzOnlKLInR7io3zZ0xOs=";
+            QuestaSetup = "sha256-0F7psE+jTimCoy+UVJRgxNC6GEVdY/PJu49hf+D7T3U=";
+        };
+        devices = {
+            arria_lite = "sha256-Epxvu1z7Z4vQWASIYEJAy5P7Meee114ZNVIAZnmTEH8=";
+            cyclone10lp = "sha256-lurSlhCuE6i2ULKNFvlWNtk6rqdvVwREC607HbMSH2I=";
+            cyclone = "sha256-lKOYy61BHxY4OyonxADg6d7IGwckGX8zu0x6dpGB5Lo=";
+            cyclonev = "sha256-1uSE/RsKR3hbyLzTGOQn1Ml5j5J26e+SmFI1hl9ry28=";
+            max10 = "sha256-gFeESwuRwrp+8rN7GYbRmOxPGDHMm+ClLRjl/rTBnOk=";
+            max = "sha256-jY/b906fJKgJOL3h5nWR5RQdvAJ3U9of6y4VopGo2z0=";
+        };
+    };
 }
